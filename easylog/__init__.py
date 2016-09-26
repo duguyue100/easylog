a8b1e95e5d204274cceaa12b793013eef177a496
Yuhuang Hu <duguyue100@gmail.com>
2016-09-26 17:38:39 +0200

    update structure

--- easylog/__init__.py
+++ easylog/__init__.py
@@ -4,11 +4,16 @@ Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
 
+from __future__ import print_function
 import os
+import sys
 from os.path import join, isdir
 
 HOME_PATH = os.environ["HOME"]
 EASYLOG_PATH = join(HOME_PATH, ".easylog")
+RUNNING_PYTHON = sys.version_info[0]
+
+print (RUNNING_PYTHON)
 
 # create resource folder if not existed.
 if not isdir(EASYLOG_PATH):
--- easylog/utils.py
+++ easylog/utils.py
@@ -3,3 +3,6 @@
 Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
+
+from __future__ import print_function
+import easylog
