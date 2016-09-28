f2fe124dea6b3d556c2c5e0b0eaca4453629a490
Yuhuang Hu <duguyue100@gmail.com>
2016-09-29 01:13:57 +0200

    general update

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# easylog
-A minimum logger for experiments.
+# EasyLog
+
+_A minimum logger for experiments._
+
+So this logger is to do some minimum things for logging an experiment.
+
+## Todo List
+
++   [ ] Decide the structure of the project
++   [ ] Use one thread for running the program and a separate thread for logging
++   [ ] Testing for speed loss by using the logger
++   [ ] Maximize the support for the applications
++   [ ] Give more analysis on the inputs (extra arguments, etc.)
+
+## Contacts
+
+Yuhuang Hu  
+Email: duguyue100@gmail.com
--- easylog/__init__.py
+++ easylog/__init__.py
@@ -9,6 +9,15 @@ import os
 import sys
 from os.path import join, isdir
 
+try:
+    import telegram
+    from telegram.bot import Bot
+    TELEGRAM_STATUS = True
+    print ("[MESSAGE] The telegram service is switched on.")
+except ImportError:
+    print ("[MESSAGE] The telegram service is switched off.")
+    TELEGRAM_STATUS = False
+
 from easylog.utils import *
 
 HOME_PATH = os.environ["HOME"]
@@ -18,3 +27,11 @@ RUNNING_PYTHON = sys.version_info[0]
 # create resource folder if not existed.
 if not isdir(EASYLOG_PATH):
     os.makedirs(EASYLOG_PATH)
+
+# setup the telegram parameters
+TELEGRAM_TOKEN_PATH = join(EASYLOG_PATH, "tokens")
+
+if not isdir(TELEGRAM_TOKEN_PATH):
+    os.makedirs(TELEGRAM_TOKEN_PATH)
+    print ("[MESSAGE] The telegram token path is create at %s" %
+           (TELEGRAM_TOKEN_PATH))
